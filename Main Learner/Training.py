94de912054c9fce47466b57c316d51ec415c20ad
Liam Watson <LiamRWatson03@gmail.com>
2023-06-15 14:39:53 -0400

    update

--- Main Learner/Training.py
+++ Main Learner/Training.py
@@ -5,7 +5,7 @@ data_name = input("Data name: ")
 model_name = input("Model name: ")
 
 Model = Model_DL.model()
-Model.load(model_name, activation_values=[4 for i in range(2)], min_diff=0.00001, learning_rate=0.000001, cycles=100, hidden_shaped=False, normaliser_depth=0)
+Model.load(model_name, activation_values=[4 for i in range(3)], min_diff=0.00001, learning_rate=0.000001, cycles=100, hidden_shaped=False, normaliser_depth=0)
 
 Data_train = Data_DL.data()
 Data_validate = Data_DL.data()
